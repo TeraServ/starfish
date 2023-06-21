65be72d0eea474fbbff999b7e98e9b4ad4a08c7d
Hitesh A H <hitesh.harikumar@teranet.ca>
2023-06-21 10:44:08 +0530

    UserService:createMultipleUsers Method

--- backend/teralearning/src/main/java/com/teranet/teralearning/service/UserService.java
+++ backend/teralearning/src/main/java/com/teranet/teralearning/service/UserService.java
@@ -19,6 +19,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -37,7 +38,7 @@ public class UserService extends UserInterface {
     }
 
     @Override
-    public ResponseEntity CreateUser(User user){
+    public ResponseEntity CreateUser(User user) {
         user.setCreatedDate(dateUtility.getDateTime());
         user.setModifiedDate(dateUtility.getDateTime());
         user.setPassword(encryptPassword("Password1!"));
@@ -45,28 +46,70 @@ public class UserService extends UserInterface {
     }
 
     @Override
-    public ResponseEntity GetAllUser(){
+    public ResponseEntity GetAllUser() {
         return new ResponseEntity(userRepository.findAll(), HttpStatus.OK);
     }
 
-    public List<userResponseDTO> getUserList() throws UserNotFoundException{
+    public List<userResponseDTO> getUserList() throws UserNotFoundException {
         List<userResponseDTO> userResponseDTOS = null;
-        try{
+        try {
             log.debug("UserService:getUserList execution started");
-            List<User> users =  userRepository.findAll();
-            if(!users.isEmpty()){
+            List<User> users = userRepository.findAll();
+            if (!users.isEmpty()) {
                 return userResponseDTOS;
-            }
-            else{
+            } else {
                 userResponseDTOS = Collections.emptyList();
                 return userResponseDTOS;
             }
 
-        } catch(Exception ex){
+        } catch (Exception ex) {
             log.error("Execution occurred while retrieving user list from database");
             throw new UserNotFoundException("Exception occurred while fetch all users from Database");
         }
 
+    }
+
+    public ResponseEntity createMultipleUsers(List<userResponseDTO> userResponseDTOS) {
+        log.debug("UserService:createMultipleUsers Init... ");
+        try {
+            if (!userResponseDTOS.isEmpty()) {
+                log.debug("UserService:createMultipleUsers started ");
+                int count = 0;
+                List<userResponseDTO> preexistentUsers = new ArrayList<>();
+                BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+                for (userResponseDTO dto : userResponseDTOS) {
+                    User user = new User();
+                    user.setFirstName(dto.getFirstname());
+                    user.setLastName(dto.getLastName());
+                    user.setEmail(dto.getEmail());
+                    user.setPhoneNumber(dto.getPhoneNumber());
+                    /*user.setStream(); Mapper for Acronym to Stream*/
+                    user.setPassword(bCryptPasswordEncoder.encode("Password1!"));
+                    user.setUserType(103);
+                    user.setCreatedDate(dateUtility.getDateTime());
+                    user.setModifiedDate(dateUtility.getDateTime());
+                    if (CreateUser(user).getStatusCode() == HttpStatus.OK) {
+                        count++;
+                        log.info("UserService:createMultipleUsers User Body created for" + user.getEmail());
+                    } else {
+                        log.info("UserService:createMultipleUsers User already Exist for" + dto.getEmail());
+                        preexistentUsers.add(dto);
+                    }
+                }
+                log.info("UserService:createMultipleUsers terminated");
+                /*Notification for Created users and PreexistentUSers*/
+                return new ResponseEntity("Multiple user created",HttpStatus.CREATED);
+
+            } else {
+                return new ResponseEntity("Blank User List", HttpStatus.BAD_REQUEST);
+            }
+
+
+        } catch (Exception ex) {
+            log.error("UserService:createMultipleUsers Exception Occurred");
+            throw new UserNotFoundException("Exception occurred while fetch user from Database");
+
+        }
     }
     public void CreateUsersFromCSV(MultipartFile file){
         log.info("UserService:CreateUsersFromCSV Init...");
