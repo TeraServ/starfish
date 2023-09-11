0a02eebd89214e696cc430197f837f05cb2c5dfc
Hitesh A H <hitesh.harikumar@teranet.ca>
2023-09-11 15:45:47 +0530

    Frontend: Resource Not Found - Logout & Navigate method

--- frontend/teralearning/src/app/resource-not-found/resource-not-found.component.ts
+++ frontend/teralearning/src/app/resource-not-found/resource-not-found.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../service/auth.service';
+import { Router } from '@angular/router';
 
+const WAIT_INTERVAL = 10; //in seconda
 @Component({
   selector: 'app-resource-not-found',
   templateUrl: './resource-not-found.component.html',
   styleUrls: ['./resource-not-found.component.scss']
 })
 export class ResourceNotFoundComponent implements OnInit {
+  
 
-  constructor(private _authService: AuthService) { }
+  constructor(private _authService: AuthService,
+    private _router: Router,) { }
 
   ngOnInit(): void {
-    // this._authService.logout();
+    this.navigateToLoginPage();
+  }
+  navigateToLoginPage(){
+    setTimeout(()=>{
+      this._authService.logout();
+      this._router.navigate(['']);
+    },WAIT_INTERVAL *1000);
+
   }
 
 }
